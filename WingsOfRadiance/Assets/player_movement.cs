1feca5d1a6828cd5a8450be443c1239b01980339
Boombostak <smokejaguar3050@hotmail.com>
2014-07-31 12:07:20 -0600

    Renamed player scripts

---
+++ WingsOfRadiance/Assets/player_movement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_movement : MonoBehaviour {
+
+    //Movement
+    public float speed; //speed, modified by various game rules
+    private float x_dir;
+    private float y_dir;
+    private Vector3 movement;
+    public bool analogue_controls; //whether the controls are analogue or, if false, digital
+
+    
+
+// Use this for initialization
+	void Start () {
+        
+	}
+	
+// Update is called once per frame
+	void Update () {
+        //Move the player based on analogue input
+        if (analogue_controls)
+        {
+            x_dir = Input.GetAxis("Horizontal");
+            y_dir = Input.GetAxis("Vertical");
+        }
+        //Move the player based on digital, boolean input
+        else
+        {
+            x_dir = Input.GetAxisRaw("Horizontal");
+            y_dir = Input.GetAxisRaw("Vertical");
+        }
+        movement = new Vector3(x_dir, y_dir, 0);
+        if (movement.magnitude > 1) 
+        { 
+            movement.Normalize(); 
+        }
+        movement = movement * speed * Time.deltaTime;
+        
+        transform.Translate(movement);
+        
+        //Debug.Log(movement.magnitude);
+
+        
+        }
+    }
---
+++ WingsOfRadiance/Assets/player_shooting.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_shooting : MonoBehaviour {
+
+    //Shooting
+    public float fire_rate; // number of shots per second
+    public float damage; //damage per shot
+    private bool shooting;
+    private float shot_countdown = 0;
+    private bool ready_to_shoot;
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        //shooting plan:
+        //shot allowed when timer is at zero
+        //start timer at zero
+        //if shooting is true and timer is zero, fire a shot, and increment shot countdown by 1/fire_rate
+        //on update, decrement countdown by 1*deltatime
+        
+        shooting = Input.GetButton("Fire1");
+
+        if (shooting && ready_to_shoot)
+        {
+            //fire a shot
+            Instantiate(player)
+
+	}
+}

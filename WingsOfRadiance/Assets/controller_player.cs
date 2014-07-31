49f0e5297eee15ac390570bbaf2a76d2ac9ba9a5
Boombostak <smokejaguar3050@hotmail.com>
2014-07-30 21:56:25 -0600

    Clamped movement works
    -Uses a workaround which I don't totally understand.

--- WingsOfRadiance/Assets/controller_player.cs
+++ WingsOfRadiance/Assets/controller_player.cs
@@ -4,24 +4,25 @@ using System.Collections;
 public class controller_player : MonoBehaviour {
 
     public float speed;
-    
-    
+    private float x_dir;
+    private float y_dir;
+    private Vector3 movement;
+    public  float diagonal_modifier;
+
     // Use this for initialization
 	void Start () {
-	
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetButton("left"))
-            transform.Translate(-speed * Time.deltaTime, 0 , 0);
-        if (Input.GetButton("right"))
-            transform.Translate(speed * Time.deltaTime, 0, 0);
-        if (Input.GetButton("down"))
-            transform.Translate(0, -speed * Time.deltaTime, 0);
-        if (Input.GetButton("up"))
-            transform.Translate(0, -speed * Time.deltaTime, 0);
-
-	}
+        
+        x_dir = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        y_dir = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+        movement = new Vector3(x_dir * diagonal_modifier, y_dir * diagonal_modifier, 0);
+        diagonal_modifier = (x_dir != 0 && y_dir != 0) ? 0.7071f : 1f ;
+        transform.Translate(movement);
+        Debug.Log(movement.magnitude);
+    }
 }

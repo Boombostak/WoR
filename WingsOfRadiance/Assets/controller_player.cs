fb72a1930edad31dc34469240b6bf31ebe8cc122
Boombostak <smokejaguar3050@hotmail.com>
2014-07-30 22:40:43 -0600

    New movement controller
    -Implemented Elliot's vector normalization.
    -Altered input manager

--- WingsOfRadiance/Assets/controller_player.cs
+++ WingsOfRadiance/Assets/controller_player.cs
@@ -3,26 +3,45 @@ using System.Collections;
 
 public class controller_player : MonoBehaviour {
 
+//Movement
     public float speed;
     private float x_dir;
     private float y_dir;
     private Vector3 movement;
     public  float diagonal_modifier;
+    public bool analogue;
 
-    // Use this for initialization
+//Shooting
+    public float fire_rate;
+
+// Use this for initialization
 	void Start () {
         
 	}
 	
-	// Update is called once per frame
+// Update is called once per frame
 	void Update () {
-
+//Move the player based on analogue input
+        if (analogue)
+        {
+            x_dir = Input.GetAxis("Horizontal");
+            y_dir = Input.GetAxis("Vertical");
+        }
+        //Move the player based on digital, boolean input
+        else
+        {
+            x_dir = Input.GetAxisRaw("Horizontal");
+            y_dir = Input.GetAxisRaw("Vertical");
+        }
+        movement = new Vector3(x_dir, y_dir, 0);
+        if (movement.magnitude > 1) 
+        { 
+            movement.Normalize(); 
+        }
+        movement = movement * speed * Time.deltaTime;
         
-        x_dir = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
-        y_dir = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
-        movement = new Vector3(x_dir * diagonal_modifier, y_dir * diagonal_modifier, 0);
-        diagonal_modifier = (x_dir != 0 && y_dir != 0) ? 0.7071f : 1f ;
         transform.Translate(movement);
+        
         Debug.Log(movement.magnitude);
     }
 }

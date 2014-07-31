4d6fbe8eb859f82823842e30d354060918dfa506
Boombostak <smokejaguar3050@hotmail.com>
2014-07-30 18:44:31 -0600

    Added first player controller script Changed input settings Added test scene

--- WingsOfRadiance/Assets/controller_player.cs
+++ WingsOfRadiance/Assets/controller_player.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class controller_player : MonoBehaviour {
 
-	// Use this for initialization
+    public float speed;
+    
+    
+    // Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        if (Input.GetButton("left"))
+            transform.Translate(-speed * Time.deltaTime, 0 , 0);
+        if (Input.GetButton("right"))
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        if (Input.GetButton("down"))
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+        if (Input.GetButton("up"))
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+
 	}
 }

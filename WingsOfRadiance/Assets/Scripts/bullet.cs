ea825dec4c864e27dc7e20e97c373c796dbaa01e
Boombostak <smokejaguar3050@hotmail.com>
2014-08-13 00:06:23 -0600

    Attempt at bullet manager
    -Not successful because tutorial was in C++, try the unity tutorial
    instead

---
+++ WingsOfRadiance/Assets/Scripts/bullet.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+static class bullet : MonoBehaviour
+{
+
+    public Vector2 position;
+    public Vector2 direction;
+    float collision_size;
+    int unique_id;
+    int active;
+    Appearance *appearance;
+    Behaviour *behaviour;
+}

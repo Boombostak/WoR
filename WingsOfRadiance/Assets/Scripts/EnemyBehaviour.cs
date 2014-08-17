058902ca0d974d92b769f77aab7a7cd6afd18b6d
Boombostak <smokejaguar3050@hotmail.com>
2014-08-17 14:00:09 -0600

    Began EnemyBehaviour script

---
+++ WingsOfRadiance/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    public float speed;
+    private Vector3 movement;
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        movement = new Vector3(0, -speed * Time.deltaTime, 0);
+
+        transform.Translate(movement);
+	}
+}

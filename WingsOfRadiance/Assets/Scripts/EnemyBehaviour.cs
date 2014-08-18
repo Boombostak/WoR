e8ae4adc34fb242bf5422e01738c770a5298027f
Boombostak <smokejaguar3050@hotmail.com>
2014-08-18 12:52:04 -0600

    New art

--- WingsOfRadiance/Assets/Scripts/EnemyBehaviour.cs
+++ WingsOfRadiance/Assets/Scripts/EnemyBehaviour.cs
@@ -5,16 +5,20 @@ public class EnemyBehaviour : MonoBehaviour {
 
     public float speed;
     private Vector3 movement;
+    private Vector3 spawnpoint;
     // Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+    void Update () {
         
         movement = new Vector3(0, -speed * Time.deltaTime, 0);
 
         transform.Translate(movement);
+        
+        
+        
 	}
 }

1ba85399b202fbea4d43ac5965c3571b5e329970
Boombostak <smokejaguar3050@hotmail.com>
2014-09-02 18:40:13 -0600

    ???

---
+++ WingsOfRadiance/Assets/Scripts/enemyspawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyspawner : MonoBehaviour {
+
+    public GameObject thing_to_spawn;
+    public float time_between_spawns;
+    public float countdown;
+    
+    // Use this for initialization
+	void Start () {
+        countdown = time_between_spawns;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        countdown = countdown - Time.deltaTime;
+        if (countdown <= 0)
+        {
+            Instantiate(thing_to_spawn);
+            countdown = time_between_spawns;
+        }
+	
+	}
+}

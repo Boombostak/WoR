ec6ff9625fc8fe6dce6554901f68e412db3f939c
Boombostak <smokejaguar3050@hotmail.com>
2014-09-24 23:36:21 -0600

    Added rudimentary loot dropper

---
+++ WingsOfRadiance/Assets/Scripts/LootDropper.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LootDropper : MonoBehaviour
+{
+    public GameObject what_to_drop;
+    private bool dropbool;
+    private int rng_drop;
+    private int rng_what;
+    public int droppercentage;
+    public GameObject[] droparray;
+
+    void Start()
+    {
+        rng_drop = Random.Range(0, 100);
+        rng_what = Random.Range(0, droparray.Length);
+            
+        if (rng_drop <= droppercentage)
+            {
+                dropbool = true;
+            }
+            
+        if (dropbool == true)
+            {
+                what_to_drop = (GameObject) droparray.GetValue(rng_what);
+                Debug.Log("Trying to drop " + what_to_drop);
+            }
+    
+    }
+
+}

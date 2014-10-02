a77bfea6a775e600ceddfae43c31526bfb449f27
Boombostak <smokejaguar3050@hotmail.com>
2014-10-01 21:58:48 -0600

    Working on PickPicker

---
+++ WingsOfRadiance/Assets/Scripts/PickPicker.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickPicker : MonoBehaviour {
+
+    private int type_length;
+    private int rarity_length;
+    private int table_length;
+    private GameObject item;
+    private int rng_type;
+    private int rng_rarity;
+    private int rng_table;
+    private int rng_item;
+    private int numberofpicks;
+    
+    public class TypePicker
+    {
+        
+    }
+
+    // Use this for initialization
+	void Start () {
+        type_length = 
+        
+        
+        
+        rng_type = Random.Range(0, type_length);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	
+
+    }
+        
+}

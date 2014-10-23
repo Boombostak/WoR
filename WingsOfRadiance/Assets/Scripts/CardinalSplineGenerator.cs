e5ac213c0498acb9e151b8150e52f27204f0653d
Boombostak <smokejaguar3050@hotmail.com>
2014-10-22 22:54:48 -0600

    Fixed spline bugs

--- WingsOfRadiance/Assets/Scripts/CardinalSplineGenerator.cs
+++ WingsOfRadiance/Assets/Scripts/CardinalSplineGenerator.cs
@@ -5,16 +5,20 @@ public class CardinalSplineGenerator : MonoBehaviour {
 
 	//P_sub_i = ((P_sub_i + 1)-(P_sub_i - 1))/alpha
 
-    public GameObject[] controlpoints = new GameObject[6];
+    public GameObject[] controlpoints;
     public Vector2 splinevector;
     public GameObject player;
     public float alignmentbias;
     public Vector3 alignmentvector;
+    public bool lock5;
+    public bool lock4;
+    public bool lock3;
+    public bool lock2;
     
 
     // Use this for initialization
 	void Start () {
-
+        
         player = GameObject.FindGameObjectWithTag("Player");
         alignmentvector.x = alignmentbias;
         alignmentvector.y = 1;
@@ -32,51 +36,46 @@ public class CardinalSplineGenerator : MonoBehaviour {
         controlpoints[0].transform.position = player.transform.position;
         controlpoints[1].transform.position = player.transform.position;
 
-        //controlpoint 5
-        if (controlpoints[5].GetComponent<FindNearestEnemy>().distance < controlpoints[5].GetComponent<FindNearestEnemy>().mindist)
+        //controlpoint 4
+        if (controlpoints[4].GetComponent<FindNearestEnemy>().distance < controlpoints[4].GetComponent<FindNearestEnemy>().mindist)
         {
-            controlpoints[5].transform.position = GetComponent<FindNearestEnemy>().closest.transform.position;
+            controlpoints[4].transform.position = GetComponent<FindNearestEnemy>().closest.transform.position;
+            lock5 = true;
         }
         else
         {
             controlpoints[5].transform.position = player.transform.position + alignmentvector + alignmentvector + alignmentvector;
         }
         
-        //controlpoint 6 follows 5
-        controlpoints[6].transform.position = controlpoints[5].transform.position;
-
-        
-        //controlpoint 4
-        if (controlpoints[4].GetComponent<FindNearestEnemy>().distance < controlpoints[4].GetComponent<FindNearestEnemy>().mindist)
-        {
-            controlpoints[4].transform.position = GetComponent<FindNearestEnemy>().closest.transform.position;
-        }
-        else
-        {
-            controlpoints[4].transform.position = player.transform.position + alignmentvector + alignmentvector;
-        }
+        //controlpoint 5 follows 4
+        controlpoints[5].transform.position = controlpoints[4].transform.position;
 
         
         //controlpoint 3
-        if (controlpoints[3].GetComponent<FindNearestEnemy>().distance < controlpoints[3].GetComponent<FindNearestEnemy>().mindist)
+        if (controlpoints[3].GetComponent<FindNearestEnemy>().distance < controlpoints[3].GetComponent<FindNearestEnemy>().mindist 
+            && lock5 == true)
         {
             controlpoints[3].transform.position = GetComponent<FindNearestEnemy>().closest.transform.position;
+            lock4 = true;
         }
         else
         {
-            controlpoints[3].transform.position = player.transform.position + alignmentvector;
+            controlpoints[3].transform.position = player.transform.position + alignmentvector + alignmentvector;
         }
 
+        
         //controlpoint 2
-        if (controlpoints[2].GetComponent<FindNearestEnemy>().distance < controlpoints[2].GetComponent<FindNearestEnemy>().mindist)
+        if (controlpoints[2].GetComponent<FindNearestEnemy>().distance < controlpoints[3].GetComponent<FindNearestEnemy>().mindist
+            && lock5 == true
+            && lock4 ==true)
         {
             controlpoints[2].transform.position = GetComponent<FindNearestEnemy>().closest.transform.position;
+            lock3 = true;
         }
         else
         {
-            controlpoints[2].transform.position = player.transform.position;
-        }
-        
+            controlpoints[2].transform.position = player.transform.position + alignmentvector;
+        }      
         
 
 	}

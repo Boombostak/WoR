e73e666db68e42453df9a23e9c1ac2a16fb42f5a
Boombostak <smokejaguar3050@hotmail.com>
2014-12-17 22:30:02 -0700

    Abstracted loot rolling
    -Isolated from bulletbehaviour
    -Add a lootroller script to enemies that drop loot.

--- WingsOfRadiance/Assets/Scripts/BulletBehaviour.cs
+++ WingsOfRadiance/Assets/Scripts/BulletBehaviour.cs
@@ -8,12 +8,17 @@ public class BulletBehaviour : MonoBehaviour
     public static float rof;
     public float firerate;
     public float self_destruct_timer;
-    public GameObject lootmanager;
+    public GameObject destroyed_enemy;
+    public GameObject item_to_drop;
+    
+    /*public GameObject lootmanager;
     public GameObject whattodrop;
     public LootWrapperClasses.LootItemType[] loottypearray;
     public LootWrapperClasses.LootItemRarity[] lootrarityarray;
     public LootWrapperClasses.LootTable[] loottablearray;
     public GameObject[] lootitemsarray;
+    
+     */
     
 
     // Use this for initialization
@@ -22,8 +27,9 @@ public class BulletBehaviour : MonoBehaviour
     {
         rof = 1 / firerate;
         Destroy(gameObject, self_destruct_timer);
-        lootmanager = GameObject.FindGameObjectWithTag("lootmanager");
-        Debug.Log(lootmanager + "is your lootmanager!");
+        
+        /*lootmanager = GameObject.FindGameObjectWithTag("lootmanager");
+        Debug.Log(lootmanager + "is your lootmanager!");*/
         
     }
 
@@ -48,6 +54,7 @@ public class BulletBehaviour : MonoBehaviour
                 Instantiate((GameObject)othercollider.gameObject.GetComponent<LootDropper>().what_to_drop, this.transform.position, this.transform.rotation);
             }*/
 
+            /*destroyed_enemy = othercollider.gameObject;
             loottypearray = lootmanager.GetComponent<LootWrapperClasses>().Llvl.lootlevel;
             lootrarityarray = loottypearray[0].itemtype;
             loottablearray = lootrarityarray[0].itemrarity;
@@ -56,12 +63,16 @@ public class BulletBehaviour : MonoBehaviour
             
             if (whattodrop != null)
             {
+                
                 Debug.Log("dropped(?)" + whattodrop);
                 Instantiate(whattodrop, this.transform.position, this.transform.rotation);
-            }
+            }*/
 
-            Debug.Log("attempted to drop" + whattodrop);
-            Destroy(othercollider.gameObject);
+            //Debug.Log("attempted to drop" + whattodrop);
+            destroyed_enemy = othercollider.gameObject;
+            item_to_drop = destroyed_enemy.GetComponent<LootRoller>().RollLoot();
+            Instantiate(item_to_drop);
+            Destroy(destroyed_enemy);
             Destroy(gameObject);
              
             
---
+++ WingsOfRadiance/Assets/Scripts/LootRoller.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LootRoller : MonoBehaviour {
+
+    public GameObject lootmanager;
+    public GameObject whattodrop;
+    public LootWrapperClasses.LootItemType[] loottypearray;
+    public LootWrapperClasses.LootItemRarity[] lootrarityarray;
+    public LootWrapperClasses.LootTable[] loottablearray;
+    public GameObject[] lootitemsarray;
+
+    public GameObject RollLoot()
+    {
+        lootmanager = GameObject.FindGameObjectWithTag("lootmanager");
+        Debug.Log(lootmanager + "is your lootmanager!");
+        loottypearray = lootmanager.GetComponent<LootWrapperClasses>().Llvl.lootlevel;
+        lootrarityarray = loottypearray[0].itemtype;
+        loottablearray = lootrarityarray[0].itemrarity;
+        lootitemsarray = loottablearray[0].items;
+        whattodrop = lootitemsarray[0];
+
+        if (whattodrop != null)
+        {
+            Debug.Log("dropped(?)" + whattodrop);
+            Instantiate(whattodrop, this.transform.position, this.transform.rotation);
+        }
+
+        Debug.Log("attempted to drop" + whattodrop);
+        return whattodrop;
+    }
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

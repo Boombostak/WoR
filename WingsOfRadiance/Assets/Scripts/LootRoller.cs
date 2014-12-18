a28d2f4bdd5a3324da0ad5d8be227f0245a4047f
Boombostak <smokejaguar3050@hotmail.com>
2014-12-17 22:43:56 -0700

    Walking away from VGAD

--- WingsOfRadiance/Assets/Scripts/LootRoller.cs
+++ WingsOfRadiance/Assets/Scripts/LootRoller.cs
@@ -5,20 +5,40 @@ public class LootRoller : MonoBehaviour {
 
     public GameObject lootmanager;
     public GameObject whattodrop;
-    public LootWrapperClasses.LootItemType[] loottypearray;
-    public LootWrapperClasses.LootItemRarity[] lootrarityarray;
-    public LootWrapperClasses.LootTable[] loottablearray;
-    public GameObject[] lootitemsarray;
+    private LootWrapperClasses.LootItemType[] loottypearray;
+    private LootWrapperClasses.LootItemRarity[] lootrarityarray;
+    private LootWrapperClasses.LootTable[] loottablearray;
+    private GameObject[] lootitemsarray;
+
+    //randomization
+
+    private int type_length;
+    private int type_rng;
+    private int rarity_length;
+    private int rarity_rng; 
+    private int table_length;
+    private int table_rng; 
+    private int items_length;
+    private int items_rng;
 
     public GameObject RollLoot()
     {
-        lootmanager = GameObject.FindGameObjectWithTag("lootmanager");
-        Debug.Log(lootmanager + "is your lootmanager!");
+        type_length = loottypearray.Length;
+        type_rng = Random.Range(0, type_length);
+        rarity_length = lootrarityarray.Length;
+        rarity_rng = Random.Range(0, rarity_length);
+        table_length = loottablearray.Length;
+        table_rng = Random.Range(0, table_length);
+        items_length = lootitemsarray.Length;
+        items_rng = Random.Range(0, items_length);
+        
+        //lootmanager = GameObject.FindGameObjectWithTag("lootmanager");
+        //Debug.Log(lootmanager + "is your lootmanager!");
         loottypearray = lootmanager.GetComponent<LootWrapperClasses>().Llvl.lootlevel;
-        lootrarityarray = loottypearray[0].itemtype;
-        loottablearray = lootrarityarray[0].itemrarity;
-        lootitemsarray = loottablearray[0].items;
-        whattodrop = lootitemsarray[0];
+        lootrarityarray = loottypearray[type_rng].itemtype;
+        loottablearray = lootrarityarray[rarity_rng].itemrarity;
+        lootitemsarray = loottablearray[table_rng].items;
+        whattodrop = lootitemsarray[items_rng];
 
         if (whattodrop != null)
         {
@@ -32,7 +52,8 @@ public class LootRoller : MonoBehaviour {
     
     // Use this for initialization
 	void Start () {
-	
+        lootmanager = GameObject.FindGameObjectWithTag("lootmanager");
+        Debug.Log(lootmanager + "is your lootmanager!");
 	}
 	
 	// Update is called once per frame

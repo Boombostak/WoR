35c03b952ea6b1d79e343772463e0a8b2ba72857
Boombostak <smokejaguar3050@hotmail.com>
2014-12-19 03:22:22 -0700

    Messed around with import settings

---
+++ WingsOfRadiance/Assets/Scripts/BackgroundBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundBehaviour : MonoBehaviour {
+
+	//vertical scrolling
+    //linking to n appropriate backgrounds
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

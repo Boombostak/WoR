4687e322fb14de2b3bce8261a02f2d509e0158d5
Boombostak <smokejaguar3050@hotmail.com>
2015-03-03 01:25:01 -0700

    Implementing Interfaces
    Code cleanup
    Object pool integration
    Still needs lots of work

---
+++ WingsOfRadiance/Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour
+{
+
+    int damage;
+    int rof;
+    public GameObject projectile;
+
+    public void Shoot()
+    {
+        Instantiate(projectile, this.transform.position, this.transform.rotation);
+    }
+}

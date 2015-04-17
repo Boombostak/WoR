c3f4f095a03697603a4180fead1b9b7712172053
Boombostak <smokejaguar3050@hotmail.com>
2015-04-17 02:23:11 -0600

    Implemented hardpoint sweeping by sine
    -Negative values reverse direction (good for symmetry)

---
+++ WingsOfRadiance/Assets/HP_Sweeper.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HP_Sweeper : MonoBehaviour {
+
+    public float _Angle;
+    public float _Period;
+
+    private float _Time;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Time = _Time + Time.deltaTime;
+        float phase = Mathf.Sin(_Time / _Period);
+        transform.localRotation = Quaternion.Euler(new Vector3(0, 0, phase * _Angle));
+    }
+}

4a450dcf11934434434861d294528abe03415c28
Boombostak <smokejaguar3050@hotmail.com>
2015-04-18 17:39:19 -0600

    Started missionbuilder script
    -And showed Sam. She said:
    "How do you make points???"
    and
    "Aren't there too many bullets???"

---
+++ WingsOfRadiance/Assets/Scripts/MissionBuilder.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissionBuilder : MonoBehaviour {
+
+	public GameObject player;
+	public PlayerTraits playertraits;
+	public GameObject startingtile;
+	public Camera camera;
+
+	void Awake(){
+				player = GameObject.FindGameObjectWithTag ("Player");
+				playertraits = player.GetComponent<PlayerTraits> ();
+		}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

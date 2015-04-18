b034c28ff8277896b646ca6f2b0fb3a369a93a0e
Boombostak <smokejaguar3050@hotmail.com>
2015-04-18 00:09:52 -0600

    Islands in the stream

--- WingsOfRadiance/Assets/Scripts/BackgroundBehaviourHoriz.cs
+++ WingsOfRadiance/Assets/Scripts/BackgroundBehaviourHoriz.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BackgroundBehaviour : MonoBehaviour {
+public class BackgroundBehaviourHoriz : MonoBehaviour {
 
 	//vertical scrolling
     //linking to n appropriate backgrounds
@@ -9,10 +9,10 @@ public class BackgroundBehaviour : MonoBehaviour {
     public GameObject[] bg_options;
     public int bg_rng;
     public GameObject bg_selection;
-    public float scrollspeed_y = 10;
+    public float scrollspeed_x = 10;
     public Vector3 offset;
     bool hasrunonce = false;
-    public float culling_y_position = -25f;
+    public float culling_x_position = 25f;
     
     // Use this for initialization
 	void Start () {
@@ -23,14 +23,14 @@ public class BackgroundBehaviour : MonoBehaviour {
 	// Update is called once per frame
     void Update()
     {
-        this.transform.Translate(0, -scrollspeed_y * Time.deltaTime, 0);
+        this.transform.Translate(scrollspeed_x * Time.deltaTime, 0, 0);
 
-        if (this.transform.position.y < culling_y_position)
+        if (this.transform.position.x > culling_x_position)
         {
             Destroy(this.gameObject);
         }
 
-        if (this.transform.position.y > culling_y_position && this.transform.position.y <0)
+        if (this.transform.position.x < culling_x_position && this.transform.position.x >0)
         {
             SpawnBG();
         }
@@ -41,7 +41,7 @@ public class BackgroundBehaviour : MonoBehaviour {
         if (!hasrunonce)
         {
             hasrunonce = true;
-            offset = new Vector3(0, this.transform.position.y + this.GetComponent<SpriteRenderer>().bounds.size.y, 0);
+            offset = new Vector3(this.transform.position.x - this.GetComponent<SpriteRenderer>().bounds.size.x, 0 ,0);
             Instantiate(bg_selection, offset, this.transform.rotation);
         }
         
